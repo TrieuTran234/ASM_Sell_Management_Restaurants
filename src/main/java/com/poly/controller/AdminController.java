4e1e23522fbb14f23e728b0e21d352e1e8c9e9f5
Trieu Tran <haitrieutran234@gmail.com>
2022-09-20 15:33:20 +0700

    Tạo account controller
    Test file jsp, ko dữ liệu

--- src/main/java/com/poly/controller/AdminController.java
+++ src/main/java/com/poly/controller/AdminController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-//@Controller
-public class AccountController {
+@Controller
+public class AdminController {
 
 	@RequestMapping("/dangKy")
 	public String dangKy() {
