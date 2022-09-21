6f893b68a698ea0b8864d2f31481ce988e063902
Trieu Tran <haitrieutran234@gmail.com>
2022-09-21 12:02:45 +0700

    kết nối với database cũ, bắt đầu code account Controller
    nhân viên và khách hàng

---
+++ src/main/java/com/poly/controller/AccountControllerForm.java
@@ -0,0 +1,33 @@
+package com.poly.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+//@Controller
+public class AccountControllerForm {
+
+	@RequestMapping("/dangKy")
+	public String dangKy() {
+		return "account/dang_Ky";
+	}
+	
+	@RequestMapping("/dangNhap")
+	public String dangNhap() {
+		return "account/dang_Nhap";
+	}
+	
+	@RequestMapping("/doiMK")
+	public String doiMK() {
+		return "account/doi_MK";
+	}
+	
+	@RequestMapping("/quenMK")
+	public String quenMK() {
+		return "account/quen_MK";
+	}
+	@RequestMapping("/suaTK")
+	public String suaTK() {
+		return "account/sua_TK";
+	}
+}
